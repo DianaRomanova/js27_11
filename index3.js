7d90ac6e7703144c982850a8b4f9d62a06e9d0fb
DianaRomanova <romanowadia@gmail.com>
2022-11-27 11:59:57 +0000

    Create index3.js

---
+++ index3.js
@@ -0,0 +1,31 @@
+let carPrice = 12000;
+const priceTOGetDiscount = 10000;
+
+if(carPrice > priceTOGetDiscount) {
+      carPrice = carPrice * 0.90;
+
+}
+//console.log(carPrice);
+
+const cartPrice = 10000;
+let deleveryPrice;
+
+if(cartPrice> 10000){
+      deleveryPrice = 0;
+
+}
+else{
+      deleveryPrice = 90;
+}
+ const fullprice = cartPrice + deleveryPrice;
+
+ //console.log(fullprice);
+
+
+ switch(arg){
+      case 1:
+            console.log('I got arg = 1');
+      case 2:
+            console.log('I got arg = 2');
+            break;      
+ }
